42b29823c13f6a9d424f3404aa0ed8f466cffae8
Zakaria <zakaria.elbourkhissi.70@lacetania.cat>
2026-05-22 22:24:15 +0200

    enllaç css

--- gen-docs.py
+++ gen-docs.py
@@ -1,9 +1,32 @@
-import os 
+import os
 import django
+import pydoc
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MY_SITE.settings')
 django.setup()
 
-import pydoc
+# Generar documentación
 pydoc.writedoc('blog.models')
 pydoc.writedoc('blog.views')
+
+# HTML generados
+files = [
+    "blog.models.html",
+    "blog.views.html"
+]
+
+# Añadir CSS automáticamente
+for file in files:
+
+    with open(file, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    content = content.replace(
+        "</head>",
+        '<link rel="stylesheet" type="text/css" href="style.css">\n</head>'
+    )
+
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(content)
+
+print("Documentació generada correctament")
